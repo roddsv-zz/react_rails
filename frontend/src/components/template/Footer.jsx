5644b1b722ce1b6848f2b4876de25e881983c301
Rodrigo Soares <adsrodsoares@gmail.com>
2021-08-01 01:08:37 -0300

    Escrevendo a frase do Rodapé

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -4,5 +4,6 @@ import React from 'react'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props =>
     <footer className="footer">
-        Componente de Rodapé
+        <span>Aplicação desenvolvida pelo candidato Rodrigo Soares para a <strong>Incentiv.me</strong>
+        </span>
     </footer>
