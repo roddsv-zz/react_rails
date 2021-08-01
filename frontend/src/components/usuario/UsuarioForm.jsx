6cf3f7779ed772ebd92d807e4e1cf050daa593a4
Rodrigo Soares <adsrodsoares@gmail.com>
2021-08-01 14:43:38 -0300

    Determinando métodos e rotas de CRUD com o axios e as funções para configurar o método HTTP específico

--- frontend/src/components/usuario/UsuarioForm.jsx
+++ frontend/src/components/usuario/UsuarioForm.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import Main from "../template/Main";
+import React, { Component } from "react"
+import Main from "../template/Main"
+import axios from "axios"
 
 const headerProps = {
     icon: 'usuarios',
@@ -7,7 +8,38 @@ const headerProps = {
     subtitle: 'Formulário de cadastro de usuários.'
 }
 
+const baseUrl = 'http://localhost:3001/usuarios'
+
+const initialState = {
+    usuario: {nome: '', email: '', senha: '', ativo: false},
+    list: []
+}
 export default class UsuarioForm extends Component {
+    
+    state = { ...initialState }
+
+    clear() {
+        this.setState({usuario: initialState.usuario})
+    }
+
+    save() {
+        const usuario = this.state.usuario
+        const method = usuario.id ? 'put' : 'post'
+        const url = usuario.id ? `${baseUrl}/${usuario.id}` : baseUrl
+        
+        axios[method](url, usuario)
+            .then(response => {
+                const list = this.getUpdatedList(response.data)
+                this.setState({usuario: initialState.usuario, list })
+            })
+    }
+
+    getUpdatedList(usuario) {
+        const list = this.state.list.filter(u => u.id !== usuario.id)
+        list.unshift(usuario)
+        return list
+    }
+
     render() {
         return (
             <Main {...headerProps}>
