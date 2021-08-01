edefd540c49fe80f52627cfc4cb2c243a2904622
Rodrigo Soares <adsrodsoares@gmail.com>
2021-07-31 22:56:13 -0300

    Criando os componentes para o template geral da página

---
+++ frontend/src/components/template/Nav.jsx
@@ -0,0 +1,8 @@
+import './Nav.css'
+import React from 'react'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props =>
+    <aside className="top-menu">
+        Menu de Navegação
+    </aside>
