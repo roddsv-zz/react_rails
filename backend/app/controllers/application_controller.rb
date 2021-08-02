bb7f2dc64133ec1f22900c683fdeebcfb8c957ec
Rodrigo Soares <adsrodsoares@gmail.com>
2021-08-02 17:12:02 -0300

    Adicionando e configurando a gem 'rack-cors' para autorizar requisições do frontend

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+
+before_action :cors_preflight_check
+after_action :cors_set_access_control_headers
+
+protected
+
+def cors_set_access_control_headers
+  headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+end
+
+# If this is a preflight OPTIONS request, then short-circuit the
+# request, return only the necessary headers and return an empty
+# text/plain.
+
+def cors_preflight_check
+  if request.method == :options
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+  end
+end
+
 end
